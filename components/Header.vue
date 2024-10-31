45347e3667959471167a7e9944fb8e0bfab8d619
ainyrihadhatul <142281874+ainyyraa125@users.noreply.github.com>
2024-10-31 09:03:58 +0700

    Add files via upload

--- components/Header.vue
+++ components/Header.vue
@@ -1,32 +1,35 @@
 <template>
-  <title>judul</title>
-    <div class="row p-3 bg-primary text-white">
-      <div class="col-12 col-lg-1">
-        <img src="~/assets/img/LOGO-SMK4 (2).png" alt="Logo SMK Negeri 4 Tasikmalaya" />
-      </div>
-
-      <div class="col-12 col-lg-10">
-        <h1>PRESENSI SISWA</h1>
-        <h2>XII PPLG 2</h2>
-        <h3>SMK Negeri 4 Tasikmalaya</h3>
-      </div>
-      
-      <div class="col-12 col-lg-1">
-        <img src="~/assets/img/logo-jurusan.png" alt="Logo Jurusan" />
-      </div>
+  <div class="row p-3 bg-primary text-white align-items-center">
+    <div class="col-12 col-lg-1 d-flex justify-content-center justify-content-lg-start">
+      <img src="~/assets/img/LOGO-SMK4 (2).png" alt="Logo SMK Negeri 4 Tasikmalaya" />
     </div>
+
+    <div class="col-12 col-lg-10 text-center text-lg-start">
+      <h1>PRESENSI SISWA</h1>
+      <h2>XII PPLG 2</h2>
+      <h3>SMK Negeri 4 Tasikmalaya</h3>
+    </div>
+
+    <div class="col-12 col-lg-1 d-flex justify-content-center justify-content-lg-end">
+      <img src="~/assets/img/logo-jurusan.png" alt="Logo Jurusan" />
+    </div>
+  </div>
 </template>
 
 <style scoped>
 img {
   width: 100%;
-  max-width: 200px; 
-  height: auto; 
+  max-width: 150px; /* Mengurangi ukuran gambar untuk tampilan lebih baik */
+  height: auto;
 }
- 
+
 h1, h2, h3 {
-    font-size: 25px; 
+  font-size: 20px; /* Ukuran font lebih kecil untuk mobile */
+}
 
+@media (min-width: 768px) {
+  h1, h2, h3 {
+    font-size: 25px; /* Ukuran font yang lebih besar untuk layar besar */
   }
-
+}
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,101 +1,100 @@
 <template>
-    <div class="container-fluid">
-      <div class=" row my-5">
-          <nuxt-link to="/logout">
-            <button type="submit"
-                class="btn btn-lg btn-danger radius kembali">
-                Logout
-              </button>
-          </nuxt-link>
-        
-        <div class="col-lg-4">
-          <nuxt-link to="/pengunjung/tambah">
-            <div class="card bg-MASUK rounded-5">
-              <div class="card-body">
-                <h2>ABSEN MASUK</h2>
-              </div>
-            </div>
-          </nuxt-link>
-        </div>
-        <div class="col-lg-4  ">
-          <nuxt-link to="/pengunjung/tambah">
-            <div class="card bg-KELUAR  rounded-5">
-              <div class="card-body">
-                <h2>ABSEN KELUAR</h2>
-              </div>
-            </div>
-          </nuxt-link>
+  <div class="container-fluid">
+    <div class="row my-5 ">
+      <div class="col-12  mb-4">
+        <nuxt-link to="/logout">
+          <button type="submit" class="btn btn-lg btn-danger radius kembali">
+            Logout
+          </button>
+        </nuxt-link>
       </div>
-      <div class="rekap col-lg-4 ">
-          <nuxt-link to="/pengunjung/rekapan">
-            <div class="card bg-Rekapan rounded-5">
-              <div class="card-body">
-                <h2>REKAPAN</h2>
-              </div>
+
+      <div class="col-lg-4 col-md-6 col-12 mb-4">
+        <nuxt-link to="/pengunjung/tambah">
+          <div class="card bg-MASUK rounded-5">
+            <div class="card-body text-center">
+              <h2>ABSEN MASUK</h2>
             </div>
-          </nuxt-link>
+          </div>
+        </nuxt-link>
+      </div>
+
+      <div class="col-lg-4 col-md-6 col-12 mb-4">
+        <nuxt-link to="/pengunjung/tambah">
+          <div class="card bg-KELUAR rounded-5">
+            <div class="card-body text-center">
+              <h2>ABSEN KELUAR</h2>
+            </div>
+          </div>
+        </nuxt-link>
+      </div>
+
+      <div class="col-lg-4 col-md-6 col-12 mb-4">
+        <nuxt-link to="/pengunjung/rekapan">
+          <div class="card bg-Rekapan rounded-5">
+            <div class="card-body text-center">
+              <h2>REKAPAN</h2>
+            </div>
+          </div>
+        </nuxt-link>
       </div>
     </div>
   </div>
-  </template>
-  
+</template>
+
 <script setup>
 useHead({
   title: "PRESENSI",
   meta: [
     {
       name: "description",
-      content: "Selamat datang diaplikasi presensi siswa digital",
+      content: "Selamat datang di aplikasi presensi siswa digital",
     },
   ],
 });
 </script>
-  <style scoped>
-  * {
-    text-decoration: none;
-  }
+
+<style scoped>
+* {
+  text-decoration: none;
+}
+.card {
+  height: 250px;
+  box-shadow: 1px 1px 10px #424242;
+}
+.card.bg-MASUK {
+  margin-top: 5%;
+  background-image: url("../assets/img/absen.png");
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+}
+.card.bg-KELUAR {
+  margin-top: 5%;
+  background-image: url("../assets/img/keluar.png");
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+}
+.card.bg-Rekapan {
+  margin-top: 5%;
+  background-image: url("../assets/img/Rekapan.jpeg");
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+}
+
+h2 {
+  color: rgb(34, 115, 207);
+  font-family: Arial, sans-serif;
+}
+
+@media (max-width: 768px) {
   .card {
-    height: 250px;
-    box-shadow: 1px 1px 10px #424242;
+    height: 200px;
   }
-  .card.bg-MASUK {
-    margin-top: 5%;
-    background-image: url("../assets/img/absen.png");
-    background-repeat: no-repeat;
-    background-position: center center;
-    background-size: cover;
-    
-  }
-  .card.bg-KELUAR {
-    margin-top: 5%;
-    background: url("../assets/img/keluar.png");
-    background-repeat: no-repeat;
-    background-position: center center;
-    background-size: cover;
-    
-  }
-  .card.bg-Rekapan {
-    margin-top: 5%;
-    /* margin-left: 80%; */
-    background: url("../assets/img/Rekapan.jpeg");
-    background-repeat: no-repeat;
-    background-position: center center;
-    background-size: cover;
-    
-  }
-
-  h2 {
-    color:rgb(34, 115, 207);
-    font: arial;
-  }
-  h1 {
-    color: white;
-  }
-  .card.bg-warning {
+  .row .col-md-6, .row .col-12 {
     margin-top: 3%;
   }
-  .card.bg-success {
-    margin-top: 3%;
-  }
-
-  </style>
+}
+</style>
